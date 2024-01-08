a278e8c13d4657ce17ce66c838de6cb1f2f7aa08
Dmitriy <neposydyakadmytro@gmail.com>
2024-01-08 20:58:08 +0200

    restyle resume section

--- src/components/sections/SectionResume.vue
+++ src/components/sections/SectionResume.vue
@@ -1,49 +1,77 @@
 <template>
-  <section id="resume" class="py-12">
-    <div class="container mx-auto px-4">
-      <p class="text-center mb-2">
-        <span class="bg-blue-500 text-white px-2">Resume</span>
+  <section id="resume" class="border-t-4">
+    <div class="container mx-auto mt-40">
+      <p class="text-center  mb-20">
+        <span class="bg-amber-400 text-white p-2">Resume</span>
       </p>
-      <h2 class="text-4xl font-semibold text-center mb-5">A summary of My Resume</h2>
-      <div class="flex flex-wrap gap-10 mt-5">
-        <div class="w-full lg:w-1/2">
-          <h2 class="text-3xl font-semibold mb-4 pb-2 border-b-2 border-blue-500">My Education</h2>
-          <div class="pl-6 border-l-2 border-blue-500">
-            <!-- Education Items -->
-            <div class="mb-4">
+      <h2 class="text-6xl font-semibold text-center mb-20">A summary of My Resume</h2>
+      <div class="grid grid-cols-2 gap-10 mb-16">
+        <div>
+          <h2 class="text-3xl font-semibold mb-5">My Education</h2>
+          <div class="border-l-2 border-amber-400 pl-16 mt-16">
+            <div class="py-5 border-b-2 border-slate-200">
+              <h3 class="text-2xl py-5">Master in Computer Engineering</h3>
+              <p class="mb-2">Harvard University / 2015 - 2017</p>
+              <p class="text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div class="py-5 border-b-2 border-slate-200">
               <h3 class="text-2xl">Master in Computer Engineering</h3>
               <p class="mb-2">Harvard University / 2015 - 2017</p>
               <p class="text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
-            <!-- More Education Items... -->
+            <div class="py-5">
+              <h3 class="text-2xl">Master in Computer Engineering</h3>
+              <p class="mb-2">Harvard University / 2015 - 2017</p>
+              <p class="text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
           </div>
         </div>
-        <div class="w-full lg:w-1/2">
-          <h2 class="text-3xl font-semibold mb-4 pb-2 border-b-2 border-blue-500">My Experience</h2>
-          <div class="pl-6 border-l-2 border-blue-500">
-            <!-- Experience Items -->
-            <div class="mb-4">
-              <h3 class="text-2xl">Sr. Front End Developer</h3>
-              <p class="mb-2">Apple Inc / 2020 - current</p>
-              <p class="text-gray-500">Lisque persius interesset his et, in quot quidam persequeris vim.</p>
+        <div>
+          <h2 class="text-3xl font-semibold mb-5">My Experience</h2>
+          <div class="border-l-2 border-amber-400 pl-16 mt-16">
+            <div class="pl-6">
+              <!-- Experience Items -->
+              <div class="mb-4">
+                <h3 class="text-2xl">Sr. Front End Developer</h3>
+                <p class="mb-2">Apple Inc / 2020 - current</p>
+                <p class="text-gray-500">Lisque persius interesset his et, in quot quidam persequeris vim.</p>
+              </div>
+              <!-- More Experience Items... -->
             </div>
-            <!-- More Experience Items... -->
           </div>
         </div>
       </div>
-      <h2 class="text-3xl font-semibold mb-4 pb-2 mt-5 border-b-2 border-blue-500">My Skills</h2>
-      <div class="flex flex-wrap gap-5">
-        <!-- Skills Items -->
-        <div class="w-full md:w-1/2">
+      <h2 class="text-3xl font-semibold mb-4 ">My Skills</h2>
+      <div class="grid grid-cols-2 gap-10 mb-16">
+        <div>
+          <div class="py-5">
+            <p class="flex justify-between mb-2">Web Design <span>65%</span></p>
+            <div class="w-full bg-gray-200 rounded-full h-2">
+              <div class="bg-blue-500 h-2 rounded-full" style="width: 65%;"></div>
+            </div>
+          </div>
+          <div class="py-5">
+            <p class="flex justify-between mb-2">Web Design <span>65%</span></p>
+            <div class="w-full bg-gray-200 rounded-full h-2">
+              <div class="bg-blue-500 h-2 rounded-full" style="width: 65%;"></div>
+            </div>
+          </div>
+          <div class="py-5">
+            <p class="flex justify-between mb-2">Web Design <span>65%</span></p>
+            <div class="w-full bg-gray-200 rounded-full h-2">
+              <div class="bg-blue-500 h-2 rounded-full" style="width: 65%;"></div>
+            </div>
+          </div>
+        </div>
+        <div>
           <p class="flex justify-between mb-2">Web Design <span>65%</span></p>
           <div class="w-full bg-gray-200 rounded-full h-2">
             <div class="bg-blue-500 h-2 rounded-full" style="width: 65%;"></div>
           </div>
         </div>
-        <!-- More Skills Items... -->
       </div>
       <p class="text-center mt-5">
-        <a class="btn border border-gray-800 rounded-none py-2 px-4" href="/path/to/resume.pdf" download="">Download CV</a>
+        <a class="btn border border-gray-800 bg-amber-400 font-semibold rounded-none py-2 px-4" href="/path/to/resume.pdf" download="">Download CV</a>
       </p>
     </div>
   </section>
@@ -57,5 +85,8 @@ export default {
 </script>
 
 <style scoped>
-
+#resume {
+  height: max-content;
+  padding-bottom: 100px;
+}
 </style>
